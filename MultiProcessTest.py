aa578479ed4fd256d6c589795089f7ccf669c2d4
MaxTurner-cloud <61795751+MaxTurner-cloud@users.noreply.github.com>
2023-01-29 11:26:20 -0500

    Rev3 commit

--- MultiProcessTest.py
+++ MultiProcessTest.py
@@ -1,42 +1,59 @@
 import pyMultiSerial as p
+import serial
 
 # Create object of class pyMultiSerial
 ms = p.MultiSerial()
-
 ms.baudrate = 9600
 ms.timeout = 2
 
-#Add Callbacks
-#Callback functions provide you an interface to perform an action at certain event.
+
+# Add Callbacks
+# Callback functions provide you an interface to perform an action at certain event.
 # Callback function on detecting a port connection.
 # Parameters: Port Number, Serial Port Object
 # Return: True if the port is to be accepted, false if the port is to be rejected based on some condition
 
 def port_connection_found_callback(portno, serial):
-    print ("Port Found: "+portno)
+    print("Port Found: " + portno)
 
 
-#register callback function
+# register callback function
 ms.port_connection_found_callback = port_connection_found_callback
 
 
 # Callback on receiving port data
 # Parameters: Port Number, Serial Port Object, Text read from port
 def port_read_callback(portno, serial, text):
-    print("Received '"+ text +"' from port "+portno)
-    serial.reset_input_buffer()
-    line = ser.readline().decode('utf-8').rstrip()
-    if line != '':
-        lineInt = int(line)
+    if text != '':
+        lineInt = int(text)
         sensor1scaled = str(lineInt / 5)  # print into window to check variables and or send
         print("Amount of water being detected:")
         print(int(sensor1scaled))
         with open('WaterPressure.txt', 'w') as f:
             f.write(dateTime)  # write to text file
             f.write(lineScaled + "\n")
+        print("Received: '" + sensor1scaled + "' at: '" + dateTime + "' from port: " + portno)
     pass
 
-#register callback function
+
+# register callback function
 ms.port_read_callback = port_read_callback
 
-                                                                                                               22,0-1        Top
+
+# Callback on port disconnection. Triggered when a device is disconnected from port.
+# Parameters: Port No
+def port_disconnection_callback(portno):
+    print("Port " + portno + " disconnected")
+
+
+# register callback function
+ms.port_disconnection_callback = port_disconnection_callback
+
+# Start Monitoring ports
+ms.Start()
+
+# To stop monitoring, press Ctrl+C in the console or command line.
+
+
+# Caution: Any code written below ms.Start() will be executed only after monitoring is stopped.
+# Make use of callback functions to execute your code.
