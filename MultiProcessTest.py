a4347dc7de782a4a72c465dcd54a591ac5cd6955
MaxTurner-cloud <61795751+MaxTurner-cloud@users.noreply.github.com>
2023-02-13 10:45:51 -0500

    Rev8 Commit

--- MultiProcessTest.py
+++ MultiProcessTest.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import paho.mqtt.publish as publish
 import pyMultiSerial as p
 import serial
 import qwiic_bme280
@@ -11,14 +12,16 @@ import requests
 ms = p.MultiSerial()
 ms.baudrate = 9600
 ms.timeout = 2
+date = str(datetime.datetime.now())
 
 
 # Add Callbacks
 # Callback functions provide you an interface to perform an action at certain event.
 # Callback function on detecting a port connection.
 # Parameters: Port Number, Serial Port Object
-# Return: True if the port is to be accepted, false if the port is to be rejected based on some condition
+# Return: True if the port is to be accepted, false if the port is to be rejected based on some condition within library
 
+# Check if there are any ports open and if so continue
 def port_connection_found_callback(portno, serial):
     print("Port Found: " + portno)
 
@@ -30,53 +33,15 @@ ms.port_connection_found_callback = port_connection_found_callback
 # Callback on receiving port data
 # Parameters: Port Number, Serial Port Object, Text read from port
 def port_read_callback(portno, serial, text):
-    mySensor = qwiic_bme280.QwiicBme280()
-    date = str(datetime.datetime.now())
-    print(text + " at: " + date + " from port: " + portno)
-    time.sleep(2)
-    if not mySensor.connected:
-        print("The Qwiic BME280 device isn't connected to the system. Please check your connection", file=sys.stderr)
-        return
+    print(text + " at: " + date + " from port: " + portno)  # pull text from the port and print it
+    time.sleep(2)  # force slowdown so pi isn't overloaded
 
-    mySensor.begin()
-    print("Humidity:\t%.3f" % mySensor.humidity)
-    print("Pressure:\t%.3f" % mySensor.pressure)
-    print("Temperature:\t%.2f" % mySensor.temperature_fahrenheit)
-    time.sleep(2)
-
-    with open('GroundWater.txt', '+a') as f:
-        f.write(date)
-        f.write("Humidity:\t%.3f" % mySensor.humidity + "\n")
-        f.write("Pressure:\t%.3f" % mySensor.pressure + "\n")
-        f.write("Temperature:\t%.2f" % mySensor.temperature_fahrenheit + "\n")
-        f.write("\n")
-    with open('GroundWater.txt', '+a') as f:
+    with open('GroundWater.txt', '+a') as f:  # write data to file GroundWater.txt stored on the pi
         f.write(date)  # write to text file
         f.write(text + "\n")
 
-    # dataList = [GMC1, GMC2, Pressure1, Pressure2]
-    # x = f.read(80)
-    # if x == "GMC1":
-    #     dataList[0] = f.readline()
-    # if x == "GMC2":
-    #     dataList[1] = f.readline()
-    # if x == "Pressure1":
-    #     dataList[2] = f.readline()
-    # if x == "Pressure2":
-    #     dataList[3] = f.readline()
-    # pass
-
-
-# def send_emoncms(dataList):
-#
-#     r = requests.post('http://172.31.171.113/input/post?node=emontx&fulljson=', json={
-#         "GMC1": dataList[0],
-#         "GMC2": dataList[1],
-#         "Pressure 1": dataList[2],
-#         "Pressure 2": dataList[3]
-#     })
-#     print(f"Status Code: {r.status_code}, Response: {r.json()}")
-
+    # here im going to parse the text data coming in and turn it into ints then scale as necessary. Some data
+    # is already scaled in the Arduinos such as percentages because it's not a lot of load for the arduino
 
 # register callback function
 ms.port_read_callback = port_read_callback
@@ -88,9 +53,53 @@ def port_disconnection_callback(portno):
     print("Port " + portno + " disconnected")
 
 
+def breakout_sensor():
+    mySensor = qwiic_bme280.QwiicBme280()
+    mySensor.begin()  # start atm breakout sensor
+    if mySensor.connected:
+        humidity = ("Humidity:\t%.3f" % mySensor.humidity)  # find humidity
+        atmPressure = ("Pressure:\t%.3f" % mySensor.pressure)  # find atmospheric pressure
+        temperature = ("Temperature:\t%.2f" % mySensor.temperature_fahrenheit)  # find temperature
+        time.sleep(2)  # force slowdown so pi isn't overloaded
+        with open('atmBreakout.txt', '+a') as f:  # write text to file with append
+            f.write(date)
+            f.write(humidity + "\n")
+            f.write(atmPressure + "\n")
+            f.write(temperature + "\n")
+            f.write("\n")
+
+    if not mySensor.connected:
+        print("The Qwiic BME280 device isn't connected to the system. Please check your connection",
+              file=sys.stderr)
+        return
+
+
+# send data to emoncms using data put into dictionary (key-value pairs)
+def emon_send():
+    # thisdict = {GMC1, GMC2, Pressure1, Pressure2}
+    # x = f.read(10)
+    # if
+    #
+    # # Finalized readings to send to emon
+    # Moisture1 = 77
+    # GPM1 = 211
+    # Temperature1 = temperature
+    # Rotation1_RPM = 0.3
+
+    # publish multiple messages - this is a Python list of dict elements!
+    # topic parts: "emon" is required; "Sprinkler1" is a Node-name; "Moisture1" "GPM1", etc are data labels
+    msg = [{'topic': "emon/Sprinkler1/Moisture1", 'payload': Moisture1},
+           {'topic': "emon/Sprinkler1/GPM1", 'payload': GPM1},
+           {'topic': "emon/Sprinkler1/Temperature1", 'payload': Temperature1},
+           {'topic': "emon/Sprinkler1/Rotation1_RPM", 'payload': Rotation1_RPM}]
+
+    # Publish them via MQTT using the multiple method
+    # Do not need to specify a host since publishing internally
+    publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
+
+
 # register callback function
 ms.port_disconnection_callback = port_disconnection_callback
 
 # Start Monitoring ports
 ms.Start()
-
