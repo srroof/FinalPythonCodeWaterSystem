fd24425779f1a220dc2015c3d1f086e1e57ade80
MaxTurner-cloud <61795751+MaxTurner-cloud@users.noreply.github.com>
2023-02-16 14:48:35 -0500

    Rev9 Commit

--- PyWaterMonitoringSystem.py
+++ PyWaterMonitoringSystem.py
@@ -33,18 +33,33 @@ ms.port_connection_found_callback = port_connection_found_callback
 # Callback on receiving port data
 # Parameters: Port Number, Serial Port Object, Text read from port
 def port_read_callback(portno, serial, text):
-    print(text + " at: " + date + " from port: " + portno)  # pull text from the port and print it
-    time.sleep(2)  # force slowdown so pi isn't overloaded
+    global gmc1, gmc2, pre1, pre2
+    # print(text)  # pull text from the port and print it
+    time.sleep(2)  # force slowdown so pi doesn't get backed up and crash
 
     with open('GroundWater.txt', '+a') as f:  # write data to file GroundWater.txt stored on the pi
         f.write(date)  # write to text file
         f.write(text + "\n")
 
+    with open('portread.txt', '+w') as r:
+        r.write(text + "\n")
+        read = r.read[0: 4]
+        if read == gmc1:
+            gmc1 = readline[4: 10]
+        if read == gmc2:
+            gmc2 = readline[4: 10]
+        if read == pre1:
+            pre1 = readline[4: 10]
+        if read == pre2:
+            pre2 = readline[4: 10]
+        else:
+            return
+
     breakout_sensor()  # calls for function breakout sensor
-    emon_send(gmc1, gmc2, pressure1, pressure2)  # calls for function breakout sensor while sending the data in as args
+    emon_send(gmc1, gmc2, pre1, pre2)  # calls for function breakout sensor while sending the data in as args
 
     # here im going to parse the text data coming in and turn it into ints then scale as necessary. Some data
-    # is already scaled in the Arduinos such as percentages because it's not a lot of load for the arduino
+    # is already scaled in the Arduino's such as percentages because it's not a lot of load for the arduino
 
 
 # register callback function
@@ -57,6 +72,13 @@ def port_disconnection_callback(portno):
     print("Port " + portno + " disconnected")
 
 
+# register callback function
+ms.port_disconnection_callback = port_disconnection_callback
+
+
+# Caution: Any code written below ms.Start() will be executed only after monitoring is stopped.
+# Make use of callback functions to execute your code.
+
 def breakout_sensor():
     mySensor = qwiic_bme280.QwiicBme280()
     mySensor.begin()  # start atm breakout sensor
@@ -64,7 +86,7 @@ def breakout_sensor():
         humidity = ("Humidity:\t%.3f" % mySensor.humidity)  # find humidity
         atm_pressure = ("Pressure:\t%.3f" % mySensor.pressure)  # find atmospheric pressure
         temperature = ("Temperature:\t%.2f" % mySensor.temperature_fahrenheit)  # find temperature
-        time.sleep(2)  # force slowdown so pi isn't overloaded
+        time.sleep(2)  # force slowdown so pi doesn't get backed up and crash
         with open('atmBreakout.txt', '+a') as f:  # write text to file with append
             f.write(date)
             f.write(humidity + "\n")
@@ -79,17 +101,17 @@ def breakout_sensor():
 
 
 # send data to emoncms using data put into dictionary (key-value pairs)
-def emon_send(gmc1, gmc2, pressure1, pressure2, humidity, atm_pressure, temperature):
+def emon_send(gmc1, gmc2, pre1, pre2, humidity, atm_pressure, temperature):
     thisdict = {
         "ground moisture 1": gmc1,
         "ground moisture 2": gmc2,
-        "Water Pressure 1": pressure1,
-        "Water Pressure 2": pressure2,
+        "Water Pressure 1": pre1,
+        "Water Pressure 2": pre2,
         "Humidity": humidity,
         "Atmospheric Pressure": atm_pressure
     }
-    x = f.read(10)
 
+    print(thisdict)
 
     # Finalized readings to send to emon
     Moisture1 = 77
@@ -98,7 +120,7 @@ def emon_send(gmc1, gmc2, pressure1, pressure2, humidity, atm_pressure, temperat
     Rotation1_RPM = 0.3
 
     # publish multiple messages - this is a Python list of dict elements!
-    # topic parts: "emon" is required; "Sprinkler1" is a Node-name; "Moisture1" "GPM1", etc are data labels
+    # topic parts: "emon" is required; "Sprinkler1" is a Node-name; "Moisture1" "GPM1", etc. are data labels
     msg = [{'topic': "emon/Sprinkler1/Moisture1", 'payload': Moisture1},
            {'topic': "emon/Sprinkler1/GPM1", 'payload': GPM1},
            {'topic': "emon/Sprinkler1/Temperature1", 'payload': Temperature1},
@@ -107,12 +129,8 @@ def emon_send(gmc1, gmc2, pressure1, pressure2, humidity, atm_pressure, temperat
     # Publish them via MQTT using the multiple method
     # Do not need to specify a host since publishing internally
     publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
-
     return
 
-
-# register callback function
-ms.port_disconnection_callback = port_disconnection_callback
 
 # Start Monitoring ports
 ms.Start()
